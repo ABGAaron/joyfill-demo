9aa5ebaf9b156dfcf7ee13e858554cca2940ddc4
ABGAaron <atrazona@myalltech.com.au>
2026-02-09 04:51:48 +0800

    fix: adding icon on field option

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+// Font Awesome setup
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCalendar,
+  faFont,
+  faAlignJustify,
+  faTable,
+  faLayerGroup,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Add icons to library
+library.add(faCalendar, faFont, faAlignJustify, faTable, faLayerGroup);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
